b12d28d6023258a7415c1b8cad7c253804f9bc40
Juho Kokko <juhokokko@Juhos-MacBook-Pro.local>
2022-11-16 17:17:19 +0200

    Exercise 4.5

--- osa-4/blogbackend/utils/list_helper.js
+++ osa-4/blogbackend/utils/list_helper.js
@@ -1,4 +1,4 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
 
@@ -6,8 +6,21 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, item) => {return sum + item.likes}, 0)
 }
 
+const favoriteblog = (blogs) => {
+  return blogs.length === 0
+    ? {}
+    : blogs.reduce((favorite, item) => {
+      if(favorite.likes >= item.likes) {
+        return { title: favorite.title, author: favorite.author, likes: favorite.likes }
+      } else {
+        return { title: item.title, author: item.author, likes: item.likes }
+      }
+    }, blogs[0])
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteblog,
 }
 

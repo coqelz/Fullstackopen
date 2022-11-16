3e18819df99f59e6b26de00e745581d9f59b8c4c
Juho Kokko <juhokokko@Juhos-MacBook-Pro.local>
2022-11-16 16:47:33 +0200

    Exersice 4.2-4.4

--- osa-4/blogbackend/app.js
+++ osa-4/blogbackend/app.js
@@ -1,5 +1,28 @@
-const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
+
+logger.info('connecting to', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('Error with MongoDB:', error.message)
+  })
+
+  app.use(express.json())
+  app.use(cors())
+  app.use(middleware.requestLogger)
+
+  app.use('/api/blogs', blogsRouter)
+
+  app.use(middleware.unknownEndpoint)
+  app.use(middleware.errorHandler)
+  module.exports = app
+  
